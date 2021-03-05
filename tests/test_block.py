cfd77f1ff54e5f5d78c14125227b9d66aa8e11a0
d.shchutskyi <d.shchutskyi@temabit.com>
2021-03-05 12:34:34 +0200

    add test

--- tests/test_block.py
+++ tests/test_block.py
@@ -25,3 +25,20 @@ class TestBlock(TestCase):
 
     def setUp(self) -> None:
         pass
+
+    def test_insufficient_args(self) -> None:
+        self.assertRaises(TypeError,
+                          FirstBlockForTesting,
+                          SecondBlockForTesting)
+
+    def test_get_list_flow(self) -> None:
+        pass
+
+    def test_set_next(self) -> None:
+        f_block = FirstBlockForTesting()
+        s_block = SecondBlockForTesting()
+        new_block = f_block.set_next(s_block)
+        self.assertTrue(new_block == s_block)
+
+    def test_handle(self):
+        pass
