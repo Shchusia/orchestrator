ec1d97378130a23bbd994cb065d875cf673a86ae
d.shchutskyi <d.shchutskyi@temabit.com>
2021-04-19 17:39:46 +0300

    up_version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 MODULE_NAME = 'orchestrator_service'
 LIB_NAME = 'orchestrator_service'
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README_PYPI.MD'), encoding='utf-8') as f:
